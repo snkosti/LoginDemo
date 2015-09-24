d37c7580a55fa1000cee814d1b3d6d5292fa3864
snkosti <sachin.koshti@gatewaytechnolabs.com>
2015-09-24 17:10:38 +0530

    LoginProj

---
+++ SampleProject1/Controller/login.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../bower_components/angular/angular.js" />
+
+angular.module('app', [])
+    .controller('loginCtrl', function ($scope) {
+        $scope.authenticate = function () {
+            debugger;
+            if($scope.UserName == 'nosql' && $scope.Password=='html5')
+            {
+                alert('Welcome to the site');
+            }
+            else
+            {
+                alert('Invalid');
+            }
+        }
+    });
